e1f880c764ad3ae2abba50672c52601f10c8a0ad
Tolege Sultan <sultantulegenov91@gmail.com>
2023-04-02 20:05:36 +0300

    Написал код по заданию 3

---
+++ hometask3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 3: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+int ReadInt(string message)
+{
+    Console.Write($"{message} >");
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+int day = ReadInt("Введите день недели");
+if (day == 6 || day == 7)
+{
+    System.Console.WriteLine("Выходной день");
+}
+else
+{
+    System.Console.WriteLine("Рабочий день");
+}
