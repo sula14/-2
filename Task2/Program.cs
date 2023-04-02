4e690fc317206ee080655393416376151d7f79ee
Tolege Sultan <sultantulegenov91@gmail.com>
2023-04-02 17:00:51 +0300

    Написал код по 2 заданию

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿int number = new Random().Next(10, 100);
+int MaxNumber(int number)
+{
+    int dec = number / 10;
+    int det = number % 10;
+    int result = dec;
+
+    if (det > result)
+    {
+        result = det;
+    }
+    return result;
+}
+System.Console.WriteLine($"Max number of {number} is {MaxNumber(number)}");
