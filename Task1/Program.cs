5623498d1566d30bd1ae135308965fff41780c2f
Tolege Sultan <sultantulegenov91@gmail.com>
2023-04-02 16:29:24 +0300

    Первая задача решена

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿int ReadInt(string message)
+{
+    Console.Write($"{message} >");
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+int number = ReadInt("Введите число");
+int lastDigit = number % 10;
+System.Console.WriteLine($"Последняя цифра числа {number} равна {lastDigit}");
