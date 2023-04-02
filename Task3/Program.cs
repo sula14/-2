4f0e424b0b54b1b6cba7601e9520de30dbaee6c8
Tolege Sultan <sultantulegenov91@gmail.com>
2023-04-02 17:25:53 +0300

    Написал код по 3 заданию

---
+++ Task3/Program.cs
@@ -0,0 +1,19 @@
+﻿int ReadInt(string message)
+{
+    Console.Write($"{message} >");
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+int num1 = ReadInt("Первое число");
+int num2 = ReadInt("Второе число");
+
+if (num1 % num2 == 0)
+{
+    System.Console.WriteLine("Кратно");
+}
+else
+{
+    int result = num1 % num2;
+    System.Console.WriteLine($"не кратно остаток {result}");
+}
