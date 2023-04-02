bc640ea655660f261eeef61e4411db4221324e7c
Tolege Sultan <sultantulegenov91@gmail.com>
2023-04-02 19:10:32 +0300

    Написал код по заданию 1

---
+++ hometask1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 1: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа. Не использовать строки для расчета (решать только при помощи операций % и /).
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int ReadInt(string message)
+{
+    Console.Write($"{message} >");
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+int number = ReadInt("Введите число");
+int result = number / 10 % 10;
+System.Console.WriteLine($"Второе число {result}");
