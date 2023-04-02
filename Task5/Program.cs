170504dabc69d2fb2a1ea82daf6045e419c63764
Tolege Sultan <sultantulegenov91@gmail.com>
2023-04-02 17:51:59 +0300

    написал код по заданию 5

---
+++ Task5/Program.cs
@@ -0,0 +1,16 @@
+﻿int ReadInt(string message)
+{
+    Console.Write($"{message} >");
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+int num = ReadInt("Введите число");
+if (num % 7 == 0 && num % 23 == 0)
+{
+    System.Console.WriteLine("да");
+}
+else 
+{
+    System.Console.WriteLine("нет");
+}
