caa366c29a719489a654b5bf20c4d043f62ca353
Tolege Sultan <sultantulegenov91@gmail.com>
2023-04-02 18:55:01 +0300

    написал код по заданию 6

---
+++ Task6/Program.cs
@@ -0,0 +1,18 @@
+﻿int num = new Random().Next(10, 10000);
+int MaxNumber(int num)
+{
+    int number = num;
+    int max = 0;
+    while(number > 0)
+    {
+    int ost = number % 10;
+    number/= 10;
+    
+    if (max < ost)
+    {
+        max = ost;
+    }
+    }
+    return max;
+}
+System.Console.WriteLine($"из чисел {num} макс число {MaxNumber(num)} ");
