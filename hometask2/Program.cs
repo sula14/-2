250754ea88796a4d270a2c57a7df3dc43a9f15ca
Tolege Sultan <sultantulegenov91@gmail.com>
2023-04-02 19:39:15 +0300

    Написал код по заданию 2

---
+++ hometask2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 2: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет. Не использовать строки для расчета (решать только при помощи операций % и /).
+
+// 645 -> 5
+
+// 78 -> третьей цифры нет
+
+// 32679 -> 6
+
+int ReadInt(string message)
+{
+    Console.Write($"{message} >");
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+int number = ReadInt("Введите число");
+int result = number % 10;
+if (number < 100)
+{
+    System.Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    System.Console.WriteLine($"Третье число {result}");
+}
